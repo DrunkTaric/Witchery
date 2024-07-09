552d4b3093e8111dc4ea887a42c2ed87c61ef057
comborush <comborush.init@proton.me>
2024-07-09 21:51:14 +0300

    responsive Nav DONE

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,8 +1,8 @@
 export default function Nav() {
   return (
-    <section className="fixed flex w-auto h-auto p-2 pl-9 pr-9 space-x-20 bg-white/20 rounded-br-2xl">
-      <img src="/images/logo_cutout.png" className="size-14" />
-      <section className="space-x-10 p-3 text-lg font-medium mt-auto mb-auto">
+    <section className="fixed flex w-full lg:w-auto h-auto p-2 pl-9 pr-9 space-x-20 bg-white/20 lg:rounded-br-2xl z-20">
+      <img src="/images/logo_cutout.png" className="size-14 ml-auto mr-auto md:ml-0 md:mr-0" />
+      <section className="hidden md:block p-3 text-lg font-medium mt-auto mb-auto space-x-10">
         <a href="#hero">Home</a>
         <a href="#about">About</a>
         <a href="#plans">Plans</a>
