3237e45aeb7913d2c9d00e4bd3a97e5dc8e66e63
comborush <comborush.init@proton.me>
2024-07-09 21:51:24 +0300

    responsive Hero DONE

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,15 +2,14 @@ import Spline from '@splinetool/react-spline';
 
 export default function Hero() {
   return (
-    <section className="grid grid-cols-2 h-[50rem]">
-      <section className="flex flex-col w-full h-full p-20">
+    <section id="hero" className="grid grid-cols-1 lg:grid-cols-2 h-[50rem]">
+      <section className="flex flex-col w-full h-full p-20 order-2 lg:order-1">
         <section className="mt-auto mb-auto space-y-5">
-          <h1 className="text-7xl font-bold">Welcome to <span className="bg-gradient-to-r from-purple-700 to-blue-800 bg-clip-text text-transparent">Venom</span></h1>
-          <h1 className="text-lg">Best Cursed Potions and Elixir company on the planet</h1>
-          <button className="p-3 pl-5 pr-5 border-2 border-purple-700 font-semibold rounded-lg hover:bg-purple-700 hover:text-black duration-500 transition-all">Explore</button>
+          <h1 className="mt-52 text-center lg:text-left text-3xl md:text-5xl lg:text-6xl font-bold">Welcome to <span className="bg-gradient-to-r from-purple-700 to-blue-800 bg-clip-text text-transparent">Venom</span></h1>
+          <h1 className="text-lg text-center lg:text-left">Best Cursed Potions and Elixir company on the planet</h1>
         </section>
       </section>
-      <section className="w-full">
+      <section className="absolute lg:relative flex w-full h-full max-h-[35rem] lg:max-h-[50rem] m-auto order-1 lg:order-2">
         <Spline scene="https://prod.spline.design/hHD6D5eABZz0Ggvl/scene.splinecode" />
       </section>
     </section>
